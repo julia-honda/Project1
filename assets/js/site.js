0b1cc7250448f9245623acb4e355b246a8f39907
juliahonda <69934533+juliahonda@users.noreply.github.com>
2021-03-13 22:33:31 -0600

    Add more variables and delete separate function

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,7 +5,22 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 
-function confirmSignUp(){
+function validationEvent() {
+  var a = document.forms["form-one"]["full-name"].value;
+  var b = document.forms["form-one"]["last-name"].value;
+  var c = document.forms["form-one"]["email"].value;
+  var d = document.forms["form-one"]["phone-num"].value;
+  var e = document.forms["form-one"]["dob"].value;
+  if (a == "" || b == "" || c == "" || d == "" || e == ""){
+    alert("All of the sections must be filled out.");
+  }
+  else{
+    alert("Welcome to the league! Details will be emailed soon.");
+  }
+}
+
+
+/*function confirmSignUp() {
   var answer = confirm("Are you ready to join the league?");
     if (answer == true) {
       alert("Welcome to the league!");
@@ -13,21 +28,4 @@ function confirmSignUp(){
     else{
       alert("We hope you join a team soon!");
     }
-}
-
-function validationEvent() {
-  var a = document.forms["form-one"]["full-name"].value;
-  var b = document.forms["form-one"]["last-name"].value;
-  var c = document.forms["form-one"]["email"].value;
-  var d = document.forms["form-one"]["phone-num"].value;
-  var e = document.forms["form-one"]["dob"].value;
-  if (a == "" || b == "" || c == ""){
-    alert("All of the sections must be filled out.")
-  }
-  else{
-    confirmSignUp();
-  }
-}
-
-  var q1 = document.getElementById('sub-button');
-  q1.addEventListener('click',validationEvent)
+}*/
